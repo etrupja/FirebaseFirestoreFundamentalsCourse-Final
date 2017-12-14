7494c3435a20de6b678e77544940d83c7d00d21d
Ervis Trupja <etrupja@bke.email>
2017-12-14 21:33:25 +0100

    other changes

--- filter.js
+++ filter.js
@@ -9,16 +9,14 @@
                 console.log('onlyMalesFilter Filter executed');
                 employeesRef.where("gender", "==", "Male")
                 .onSnapshot(function(querySnapshot) {
-                    EmptyTable();
-                    LoadFilteredData(querySnapshot);
+                    LoadTableData(querySnapshot);
                 });
             });
 
             $('#fullTimeFilter').click(function(){
                 employeesRef.where("isFullTime", "==", true)
                     .onSnapshot(function(querySnapshot) {
-                        EmptyTable();
-                        LoadFilteredData(querySnapshot);
+                        LoadTableData(querySnapshot);
                 });
             });
 
@@ -26,8 +24,7 @@
                 //older than 30
                 employeesRef.where("age", ">=", 30)
                 .onSnapshot(function(querySnapshot) {
-                    EmptyTable();
-                    LoadFilteredData(querySnapshot);
+                    LoadTableData(querySnapshot);
                 });
             });
 
@@ -35,8 +32,7 @@
                 //older than 35, but younger than 50
                 employeesRef.where("age", ">=", 35).where("age", "<=", 50)
                 .onSnapshot(function(querySnapshot) {
-                    EmptyTable();
-                    LoadFilteredData(querySnapshot);
+                    LoadTableData(querySnapshot);
                 });
             });
 
@@ -45,18 +41,17 @@
                 employeesRef.where("gender", "==", "Female")
                 employeesRef.where("yearsOfExperience", ">=", 5).where("yearsOfExperience", "<=", 10)
                 .onSnapshot(function(querySnapshot) {
-                    EmptyTable();
-                    LoadFilteredData(querySnapshot);
+                    LoadTableData(querySnapshot);
                 });
             });
 
             $('#clearFilter').click(function(){
-                LoadData();
+                employeesRef.get().then(function(querySnapshot) {
+                    LoadTableData(querySnapshot);
+                });
             });
-
-
             
-            function LoadFilteredData(querySnapshot){
+            function LoadTableData(querySnapshot){
                 var tableRow='';
                 querySnapshot.forEach(function(doc) {
                     var document = doc.data();
@@ -72,32 +67,6 @@
                     tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
                     tableRow += '</tr>';
                 });
-                $('tbody.tbodyData').append(tableRow);
-            }
-
-            function LoadData(){
-                EmptyTable();
-                employeesRef.get().then(function(querySnapshot) {
-                    var tableRow='';
-                    querySnapshot.forEach(function(doc) {
-                        var document = doc.data();
-                        tableRow +='<tr>';
-                        tableRow += '<td class="fname">' + document.fName + '</td>';
-                        tableRow += '<td class="lname">' + document.lName + '</td>';
-                        tableRow += '<td class="email">' + document.email + '</td>';
-                        tableRow += '<td class="age">' + document.age + '</td>';
-                        tableRow += '<td class="gender">' + document.gender + '</td>';
-                        tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
-                        tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
-                        tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
-                        tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
-                        tableRow += '</tr>';
-                    });
-                    $('tbody.tbodyData').append(tableRow);
-                });
-            }
-        
-            function EmptyTable(){
-            $('tbody.tbodyData').empty();
+                $('tbody.tbodyData').html(tableRow);
             }
 });
--- script.js
+++ script.js
@@ -107,35 +107,38 @@ $(document ).ready(function() {
 
     $("#searchEmployee" ).change(function() {
         console.log('You entered: ', $(this).val());
+        //Get the Employee Data
+        var searchValue = $(this).val()
+        employeesRef.where("fName", "==", searchValue)
+        .onSnapshot(function(querySnapshot) {
+            LoadTableData(querySnapshot)
+        });
       });
 
 
 
       function LoadData(){
-        EmptyTable();
-
         employeesRef.get().then(function(querySnapshot) {
-            var tableRow='';
-            querySnapshot.forEach(function(doc) {
-                var document = doc.data();
-                tableRow +='<tr>';
-                tableRow += '<td class="fname">' + document.fName + '</td>';
-                tableRow += '<td class="lname">' + document.lName + '</td>';
-                tableRow += '<td class="email">' + document.email + '</td>';
-                tableRow += '<td class="age">' + document.age + '</td>';
-                tableRow += '<td class="gender">' + document.gender + '</td>';
-                tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
-                tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
-                tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
-                tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
-                tableRow += '</tr>';
-            });
-            $('tbody.tbodyData').append(tableRow);
+            LoadTableData(querySnapshot)
         });
       }
 
-      function EmptyTable(){
-        $('tbody.tbodyData').empty();
+      function LoadTableData(querySnapshot){
+        var tableRow='';
+        querySnapshot.forEach(function(doc) {
+            var document = doc.data();
+            tableRow +='<tr>';
+            tableRow += '<td class="fname">' + document.fName + '</td>';
+            tableRow += '<td class="lname">' + document.lName + '</td>';
+            tableRow += '<td class="email">' + document.email + '</td>';
+            tableRow += '<td class="age">' + document.age + '</td>';
+            tableRow += '<td class="gender">' + document.gender + '</td>';
+            tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
+            tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
+            tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
+            tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
+            tableRow += '</tr>';
+        });
+        $('tbody.tbodyData').html(tableRow);
       }
-
 });
