0772348f35872b0125053548db7c46f46841c94e
Ervis Trupja <etrupja@bke.email>
2017-12-14 18:47:43 +0100

    CRUD

--- filter.js
+++ filter.js
@@ -1,24 +1,103 @@
 
+    // Initialize Cloud Firestore through Firebase
+    var db = firebase.firestore();
+    var employeesRef = db.collection("employees");
     
-    $(document ).ready(function() {
-            // filtering functionalities
+    $(document).ready(function() {
+
             $('#onlyMalesFilter').click(function(){
                 console.log('onlyMalesFilter Filter executed');
-            });
-            $('#fullTimeFilter').click(function(){
-                console.log('fullTimeFilter Filter executed');
-            });
-            $('#olderThenFilter').click(function(){
-                console.log('olderThenFilter Filter executed');
-            });
-            $('#ageBetweenFilter').click(function(){
-                console.log('ageBetweenFilter Filter executed');
-            });
-            $('#avgFilter').click(function(){
-                console.log('avgFilter Filter executed');
-            });
-            $('#clearFilter').click(function(){
-                console.log('clearFilter Filter executed');
+                employeesRef.where("gender", "==", "Male")
+                .onSnapshot(function(querySnapshot) {
+                    EmptyTable();
+                    LoadFilteredData(querySnapshot);
+                });
             });
 
+            $('#fullTimeFilter').click(function(){
+                employeesRef.where("isFullTime", "==", true)
+                    .onSnapshot(function(querySnapshot) {
+                        EmptyTable();
+                        LoadFilteredData(querySnapshot);
+                });
+            });
+
+            $('#olderThenFilter').click(function(){
+                //older than 30
+                employeesRef.where("age", ">=", 30)
+                .onSnapshot(function(querySnapshot) {
+                    EmptyTable();
+                    LoadFilteredData(querySnapshot);
+                });
+            });
+
+            $('#ageBetweenFilter').click(function(){
+                //older than 35, but younger than 50
+                employeesRef.where("age", ">=", 35).where("age", "<=", 50)
+                .onSnapshot(function(querySnapshot) {
+                    EmptyTable();
+                    LoadFilteredData(querySnapshot);
+                });
+            });
+
+            $('#yearsOfExperienceFilter').click(function(){
+                //female and 5-10 years of experience
+                employeesRef.where("gender", "==", "Female")
+                employeesRef.where("yearsOfExperience", ">=", 5).where("yearsOfExperience", "<=", 10)
+                .onSnapshot(function(querySnapshot) {
+                    EmptyTable();
+                    LoadFilteredData(querySnapshot);
+                });
+            });
+
+            $('#clearFilter').click(function(){
+                LoadData();
+            });
+
+
+            
+            function LoadFilteredData(querySnapshot){
+                var tableRow='';
+                querySnapshot.forEach(function(doc) {
+                    var document = doc.data();
+                    tableRow +='<tr>';
+                    tableRow += '<td class="fname">' + document.fName + '</td>';
+                    tableRow += '<td class="lname">' + document.lName + '</td>';
+                    tableRow += '<td class="email">' + document.email + '</td>';
+                    tableRow += '<td class="age">' + document.age + '</td>';
+                    tableRow += '<td class="gender">' + document.gender + '</td>';
+                    tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
+                    tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
+                    tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
+                    tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
+                    tableRow += '</tr>';
+                });
+                $('tbody.tbodyData').append(tableRow);
+            }
+
+            function LoadData(){
+                EmptyTable();
+                employeesRef.get().then(function(querySnapshot) {
+                    var tableRow='';
+                    querySnapshot.forEach(function(doc) {
+                        var document = doc.data();
+                        tableRow +='<tr>';
+                        tableRow += '<td class="fname">' + document.fName + '</td>';
+                        tableRow += '<td class="lname">' + document.lName + '</td>';
+                        tableRow += '<td class="email">' + document.email + '</td>';
+                        tableRow += '<td class="age">' + document.age + '</td>';
+                        tableRow += '<td class="gender">' + document.gender + '</td>';
+                        tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
+                        tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
+                        tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
+                        tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
+                        tableRow += '</tr>';
+                    });
+                    $('tbody.tbodyData').append(tableRow);
+                });
+            }
+        
+            function EmptyTable(){
+            $('tbody.tbodyData').empty();
+            }
 });
--- initialize.js
+++ initialize.js
@@ -2,167 +2,128 @@
         var db = firebase.firestore();
         var employeesRef = db.collection("employees");
 
-        employeesRef.doc("etr@gmail.com").set({
-            fName: "Ervis", 
-            lName: "Trupja", 
-            email: "etr@gmail.com",
-            age: 25, 
-            gender: 'male',
-            average: 4.9,
-            isFullTime: true,
-        });
-        employeesRef.doc("str@gmail.com").set({
-            fName: "Suada", 
-            lName: "Trupja", 
-            email: "str@gmail.com",
-            age: 17, 
-            gender: 'female',
-            average: 5.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("ert@gmail.com").set({
-            fName: "Ermal", 
-            lName: "Trupja", 
-            email: "ert@gmail.com",
-            age: 28, 
-            gender: 'male',
-            average: 4.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("otr@gmail.com").set({
-            fName: "Ornela", 
-            lName: "Trupja", 
-            email: "otr@gmail.com",
-            age: 23, 
-            gender: null,
-            average: 4.0,
-            isFullTime: false,
-        });
+        // employeesRef.doc("R.Dikles").set({
+        //     fName: "Ranice", 
+        //     lName: "Dikles", 
+        //     email: "rdikles0@hatena.ne.jp",
+        //     age: 39, 
+        //     gender: 'Female',
+        //     yearsOfExperience: 9,
+        //     isFullTime: true,
+        // });
 
-        employeesRef.doc("a@gmail.com").set({
-            fName: "Ervis", 
-            lName: "Trupja", 
-            email: "etr@gmail.com",
-            age: 25, 
-            gender: 'male',
-            average: 4.9,
-            isFullTime: true,
-        });
-        employeesRef.doc("b@gmail.com").set({
-            fName: "Suada", 
-            lName: "Trupja", 
-            email: "str@gmail.com",
-            age: 17, 
-            gender: 'female',
-            average: 5.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("q@gmail.com").set({
-            fName: "Ermal", 
-            lName: "Trupja", 
-            email: "ert@gmail.com",
-            age: 28, 
-            gender: 'male',
-            average: 4.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("w@gmail.com").set({
-            fName: "Ornela", 
-            lName: "Trupja", 
-            email: "otr@gmail.com",
-            age: 23, 
-            gender: null,
-            average: 4.0,
-            isFullTime: false,
-        });
+        // employeesRef.doc("A.Lamberton").set({
+        //     fName: "Adler", 
+        //     lName: "Lamberton", 
+        //     email: "alamberton1@ft.com",
+        //     age: 50, 
+        //     gender: 'Male',
+        //     yearsOfExperience: 7,
+        //     isFullTime: true,
+        // });
 
-        employeesRef.doc("e@gmail.com").set({
-            fName: "Ervis", 
-            lName: "Trupja", 
-            email: "etr@gmail.com",
-            age: 25, 
-            gender: 'male',
-            average: 4.9,
-            isFullTime: true,
-        });
-        employeesRef.doc("r@gmail.com").set({
-            fName: "Suada", 
-            lName: "Trupja", 
-            email: "str@gmail.com",
-            age: 17, 
-            gender: 'female',
-            average: 5.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("t@gmail.com").set({
-            fName: "Ermal", 
-            lName: "Trupja", 
-            email: "ert@gmail.com",
-            age: 28, 
-            gender: 'male',
-            average: 4.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("z@gmail.com").set({
-            fName: "Ornela", 
-            lName: "Trupja", 
-            email: "otr@gmail.com",
-            age: 23, 
-            gender: null,
-            average: 4.0,
-            isFullTime: false,
-        });
+        // employeesRef.doc("J.Summerson").set({
+        //     fName: "Jeremy", 
+        //     lName: "Summerson", 
+        //     email: "jsummerson2@aboutads.info",
+        //     age: 58, 
+        //     gender: 'Male',
+        //     yearsOfExperience: 6,
+        //     isFullTime: true,
+        // });
 
-        employeesRef.doc("u@gmail.com").set({
-            fName: "Ervis", 
-            lName: "Trupja", 
-            email: "etr@gmail.com",
-            age: 25, 
-            gender: 'male',
-            average: 4.9,
-            isFullTime: true,
-        });
-        employeesRef.doc("i@gmail.com").set({
-            fName: "Suada", 
-            lName: "Trupja", 
-            email: "str@gmail.com",
-            age: 17, 
-            gender: 'female',
-            average: 5.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("o@gmail.com").set({
-            fName: "Ermal", 
-            lName: "Trupja", 
-            email: "ert@gmail.com",
-            age: 28, 
-            gender: 'male',
-            average: 4.0,
-            isFullTime: false,
-        });
-        employeesRef.doc("p@gmail.com").set({
-            fName: "Ornela", 
-            lName: "Trupja", 
-            email: "otr@gmail.com",
-            age: 23, 
-            gender: null,
-            average: 4.0,
-            isFullTime: false,
-        });
+        // employeesRef.doc("G.O'Byrne").set({
+        //     fName: "Gunilla", 
+        //     lName: "O'Byrne", 
+        //     email: "gobyrne3@bbc.co.uk",
+        //     age: 56, 
+        //     gender: 'Female',
+        //     yearsOfExperience: 14,
+        //     isFullTime: true,
+        // });
 
+        // employeesRef.doc("W.Ells").set({
+        //     fName: "Weston", 
+        //     lName: "Ells", 
+        //     email: "wells4@linkedin.com",
+        //     age: 60, 
+        //     gender: 'Male',
+        //     yearsOfExperience: 8,
+        //     isFullTime: false,
+        // });
 
-        db.collection("employees").get().then(function(querySnapshot) {
+        // employeesRef.doc("F.Penhall").set({
+        //     fName: "Freddy", 
+        //     lName: "Penhall", 
+        //     email: "fpenhall5@blogspot.com",
+        //     age: 50, 
+        //     gender: 'Female',
+        //     yearsOfExperience: 8,
+        //     isFullTime: true,
+        // });
+
+        // employeesRef.doc("M.Pothergill").set({
+        //     fName: "Marion", 
+        //     lName: "Pothergill", 
+        //     email: "mpothergill6@theglobeandmail.com",
+        //     age: 39, 
+        //     gender: 'Male',
+        //     yearsOfExperience: 14,
+        //     isFullTime: false,
+        // });
+        
+        // employeesRef.doc("P.Leechman").set({
+        //     fName: "Prinz", 
+        //     lName: "Leechman", 
+        //     email: "pleechman7@ehow.com",
+        //     age: 58, 
+        //     gender: 'Male',
+        //     yearsOfExperience: 14,
+        //     isFullTime: true,
+        // });
+
+        // employeesRef.doc("B.Newby").set({
+        //     fName: "Babita", 
+        //     lName: "Newby", 
+        //     email: "bnewby9@techcrunch.com",
+        //     age: 41, 
+        //     gender: 'Female',
+        //     yearsOfExperience: 10,
+        //     isFullTime: false,
+        // });
+
+        // employeesRef.doc("C.Blest").set({
+        //     fName: "Celestia", 
+        //     lName: "Blest", 
+        //     email: "cblesta@delicious.com",
+        //     age: 19, 
+        //     gender: 'Female',
+        //     yearsOfExperience: 1,
+        //     isFullTime: false,
+        // });
+        
+        // employeesRef.doc("N.Tather").set({
+        //     fName: "Nannette", 
+        //     lName: "Tather", 
+        //     email: "ntatherb@delicious.com",
+        //     age: 40, 
+        //     gender: 'Female',
+        //     yearsOfExperience: 13,
+        //     isFullTime: true,
+        // });
+
+        employeesRef.get().then(function(querySnapshot) {
             var tableRow='';
             querySnapshot.forEach(function(doc) {
                 var document = doc.data();
                 tableRow +='<tr>';
-                tableRow += '<td>' + document.fName + '</td>';
-                tableRow += '<td>' + document.lName + '</td>';
-                tableRow += '<td>' + document.email + '</td>';
-                tableRow += '<td>' + document.age + '</td>';
-                tableRow += '<td>' + document.gender + '</td>';
-                tableRow += '<td>' + document.average + '</td>';
-                tableRow += '<td>' + document.isFullTime + '</td>';
+                tableRow += '<td class="fname">' + document.fName + '</td>';
+                tableRow += '<td class="lname">' + document.lName + '</td>';
+                tableRow += '<td class="email">' + document.email + '</td>';
+                tableRow += '<td class="age">' + document.age + '</td>';
+                tableRow += '<td class="gender">' + document.gender + '</td>';
+                tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
+                tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
                 tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
                 tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
                 tableRow += '</tr>';
